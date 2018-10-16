953768e50c360832a7fc4fa261515c58e6e7c0cf
Roger Wilco <recraft@pm.me>
2018-10-16 21:37:55 +0200

    add more content

--- init.R
+++ init.R
@@ -6,8 +6,12 @@ image_read("images/logo.png") %>%
   image_trim %>%
   image_background("white") %>%
   image_convert(format = "png", depth = 8) %>%
-image_resize("x32") %>%
+image_resize("x64") %>%
   image_write("images/favicon.png")
+
+image_read("images/terc.png") %>%
+  image_convert(format = "png", depth = 8) %>%
+  image_write("images/terc.png")
 
 system("icotool -o favicon.ico -c images/favicon.png")
 system("icotool -o images/favicon.ico -c images/favicon.png")
